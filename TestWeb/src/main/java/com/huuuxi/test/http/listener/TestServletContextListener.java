a84087f90b2538aece5f9e5fda50dd1b6a55cd89
huuuxi <huuuxi@gmail.com>
2014-03-25 16:08:10 +0800

    webapp add

---
+++ TestWeb/src/main/java/com/huuuxi/test/http/listener/TestServletContextListener.java
@@ -0,0 +1,26 @@
+package com.huuuxi.test.http.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * @author wyliujiangbin
+ *##	Listener
+ *	分三类：ServletContext；HttpSessionListener ；HttpRequestListener ；
+ *	按照属性来分：Attribute；Event；
+ */
+public class TestServletContextListener implements	ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
